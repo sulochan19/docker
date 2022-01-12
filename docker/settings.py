7f456676c7fd64ff661929f2c27a9476cd52323a
sulochan <sulochan.st@gmail.com>
2022-01-12 11:18:36 +0545

    add postgres as database

--- docker/settings.py
+++ docker/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'docker.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dockertest',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
